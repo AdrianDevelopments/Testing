1514cc3d1ceef99e7e200c678e6177aa8f0cdf02
Adrian <adferrer0611@gmail.com>
2023-09-21 10:45:27 -0400

    Terminando

--- Automation/Prueba/test.js
+++ Automation/Prueba/test.js
@@ -1,13 +1,7 @@
 const { Builder, By, Key, until } = require("selenium-webdriver");
-<<<<<<< HEAD
-const ElementId = require("./calcular")
-=======
 const ElementId = require("./calcular");
->>>>>>> 42d9fecc38ba4bcf8b6d1b886a52c02ed3bcfcc7
 const edge = require("selenium-webdriver/edge");
-const chrome = require("selenium-webdriver/chrome");
 const edgeDriver = require("edgedriver");
-const chromeDriver = require("chromedriver");
 
 const ExecuteTest = async () => {
     let service = await new edge.ServiceBuilder(edgeDriver.findEdgePath);
@@ -16,29 +10,6 @@ const ExecuteTest = async () => {
         .setEdgeService(service)
         .build();
     try {
-<<<<<<< HEAD
-    
-     await driver.get("https://forms.gle/mzXctH9o5rGGk7cy7");
-
-    const elementId = new ElementId();
-    elementId.buildElementArray();
-
-    elementId.ElementArray.forEach(async(item) =>{
-        const radioButton = await driver.findElement(By.id(item))
-        await driver.wait(until.elementIsEnabled(radioButton))
-        await radioButton.click()
-    })
-
-    const button = await driver
-        .findElement(By.css(".uArJ5e.UQuaGc.Y5sE8d.VkkpIf.QvWxOd")).click()
-        
-
-    } catch (error) {
-        console.error(error)
-    }finally{
-        
-    }  
-=======
         await driver.get("https://forms.gle/mzXctH9o5rGGk7cy7");
 
         const elementId = new ElementId();
@@ -63,7 +34,6 @@ const ExecuteTest = async () => {
             driver.quit();
         }, 3000);
     }
->>>>>>> 42d9fecc38ba4bcf8b6d1b886a52c02ed3bcfcc7
 };
 
 const CountTime = () => {
