ad92f6a0dd7fbf69194ce0d9a32001b6f3f474de
Adrián Saul Polanco Ferrer <Adrian>
2023-10-03 22:38:13 -0400

    Login Screen terminado

--- Automation/Prueba/electron-ui-app/src/renderer/src/components/fields.jsx
+++ Automation/Prueba/electron-ui-app/src/renderer/src/components/fields.jsx
@@ -1,9 +1,25 @@
-/* eslint-disable react/prop-types */
+import { useState } from "react";
+import  SeeIcon  from "../components/seeIcon.jsx";
 
-const Field = ({ placeholder, stylesPropsContainer, stylesPropsInput }) => {
-  return (
-    <div className={`${stylesPropsContainer}`}>
-      <input type="text" placeholder={placeholder} className={`border-solid border rounded-md w-full ${stylesPropsInput}`} />
+const Field = ({ typeInput, placeholder, stylesPropsContainer, stylesPropsInput}) => {
+  const [seePassword, setSeePassword] = useState(true)
+
+  return typeInput !== "password" ? 
+   (
+    <div className={`flex flex-row ${stylesPropsContainer}`}>
+      <input type={typeInput} placeholder={placeholder} className={`border-solid border rounded-md w-full ${stylesPropsInput}`} /> 
+    </div>
+  ) :
+  (
+    seePassword?
+    <div className={`flex flex-row ${stylesPropsContainer}`}>
+      <input type={"password"} placeholder={placeholder} className={`border-solid border border-r-0 rounded-l-md w-full ${stylesPropsInput}`} /> 
+      <SeeIcon seePassword={seePassword} setSeePassword={setSeePassword}></SeeIcon>
+    </div>
+    :
+    <div className={`flex flex-row ${stylesPropsContainer}`}>
+      <input type={"text"} placeholder={placeholder} className={`border-solid border border-r-0 rounded-l-md w-full ${stylesPropsInput}`} /> 
+      <SeeIcon seePassword={seePassword} setSeePassword={setSeePassword}></SeeIcon>
     </div>
   )
 }
