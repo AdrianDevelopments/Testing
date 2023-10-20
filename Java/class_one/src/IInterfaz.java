99ee58b91fa79b8086589f0c30130ac56a7bc872
Adrian <adferrer0611@gmail.com>
2023-10-19 20:06:56 -0400

    Updated repository

--- Java/class_one/src/IInterfaz.java
+++
@@ -1,6 +0,0 @@
-public interface IInterfaz {
-    //El especificador de acceso default es solo accesible desde el mismo paquete
-    //En Java se sigue el camelCase, por lo que los metodos de varias palabras, cada palabra despues de la primera empezara con mayuscula
-    default void defaultMetodo() {
-    }
-}
